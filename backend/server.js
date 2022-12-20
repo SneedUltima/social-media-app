7349d8b9a5dabe2fb3bfd404736a249e3d7c2b31
Dylan <dylan.cobham@yahoo.com.au>
2022-12-20 16:23:30 +1030

    build: create server and connect to mongodb

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Unable to connect to db");
+  });
