c7f4cec0cc13888a57c12988e11a69035d83aeef
Dylan <dylan.cobham@yahoo.com.au>
2022-12-24 23:00:37 +1030

    fix: home page route now protected with a redirect if no user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,18 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { user } = useContext(AuthContext);
+
   return (
     <div>
       <BrowserRouter>
         <div>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={user ? <Home /> : <Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
           </Routes>
