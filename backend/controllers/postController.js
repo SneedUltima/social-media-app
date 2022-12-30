efd7ef8afa26d4fc7a25785826e139c9e9d9d8e6
Dylan <dylan.cobham@yahoo.com.au>
2022-12-30 20:39:18 +1030

    build: user press on like button now add 1 like to post total

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -2,14 +2,14 @@ const Post = require("../models/postModel");
 const mongoose = require("mongoose");
 
 const createPost = async (req, res) => {
-  const { text, author } = req.body;
+  const { text, author, likes } = req.body;
 
   if (!text || !author) {
     return res.status(400).json({ error: "Please fill in all fields" });
   }
 
   try {
-    const createdPost = await Post.create({ text, author });
+    const createdPost = await Post.create({ text, author, likes });
     res.status(200).json(createdPost);
   } catch (error) {
     res.status(400).json({ error: error.Post });
@@ -59,7 +59,12 @@ const updatePost = async (req, res) => {
     return res.status(404).json({ error: "No such post" });
   }
 
-  const post = await Post.findOneAndUpdate({ _id: id }, { ...req.body });
+  const post = await Post.findOneAndUpdate(
+    { _id: id },
+    {
+      $inc: { likes: 1 },
+    }
+  );
 
   if (!post) {
     return res.status(404).json({ error: "No such post" });
--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import formatRelative from "date-fns/formatRelative";
 import { subDays } from "date-fns/esm";
+import { useState } from "react";
+import {
+  ChatBubbleBottomCenterTextIcon,
+  HandThumbUpIcon,
+} from "@heroicons/react/24/outline";
+import { HandThumbUpIcon as HandThumbsUpIconSolid } from "@heroicons/react/24/solid";
 
 const Post = ({ post }) => {
+  const [like, setLike] = useState(false);
+  const [likes, setLikes] = useState(post.likes);
+
+  const handleLike = async (e) => {
+    e.preventDefault();
+    setLike((like) => !like);
+
+    const likeCount = { likes: 1 };
+
+    const response = await fetch("/posts/" + post._id, {
+      method: "PATCH",
+      body: JSON.stringify(likeCount),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      setLikes(likes + 1);
+    }
+  };
+
   return (
     <div className="mx-5 sm:mx-10 mt-6 sm:3/5 md:w-3/5">
       <div className="boxShadow py-2 px-2 w-full h-fit resize-none rounded-xl bg-odin-blue border-odin-gold text-odin-white">
@@ -15,14 +43,27 @@ const Post = ({ post }) => {
         </div>
         <div className="mb-4">{post.text}</div>
         <div className="mb-4">
-          <div>{post.likes} Likes</div>
+          <div>{likes} Likes</div>
         </div>
         <div className="flex justify-between gap-4">
-          <button className=" bg-odin-green text-odin-white border-none w-20 font-bold rounded hover:bg-odin-green/75 flex-1 py-1">
-            Like
+          <button
+            className=" bg-odin-green text-odin-white border-none w-20 font-bold rounded hover:bg-odin-green/75 flex-1 py-1"
+            onClick={handleLike}
+          >
+            <div className="flex justify-center gap-1">
+              {!like ? (
+                <HandThumbUpIcon className="w-5" />
+              ) : (
+                <HandThumbsUpIconSolid className="w-5" />
+              )}
+              {!like ? "Like" : "Liked"}
+            </div>
           </button>
           <button className=" bg-odin-green text-odin-white border-none w-20 font-bold rounded hover:bg-odin-green/75 flex-1 py-1">
-            Comment
+            <div className="flex justify-center gap-1">
+              <ChatBubbleBottomCenterTextIcon className="w-5" />
+              <p>Comment</p>
+            </div>
           </button>
         </div>
       </div>
